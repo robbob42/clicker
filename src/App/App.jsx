80f84eebca685b8ae550afa0a28a71026ee1ae89
Olsen <olsen@ynab.robandmindy.com>
2019-04-06 09:57:31 -0400

    Add button and click count

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,10 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Button, Typography, Paper } from '@material-ui/core';
 
 function App() {
+  const [clickCount, updateClickCount] = useState(0);
+
+  const click = () => {
+    updateClickCount(clickCount + 1);
+  };
+
   return (
-    <div>
-      Clicker App
-    </div>
+    <>
+      <Paper>
+        <Button
+          color="primary"
+          variant="contained"
+          onClick={click}
+        >
+          Click Me!
+        </Button>
+        <Typography>
+          {clickCount}
+        </Typography>
+      </Paper>
+    </>
   );
 }
 
